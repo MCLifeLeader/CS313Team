f3ee3a19f9e3bb9ffb08566c8a7f5f999f933de9
Michael Carey <kb7ppb@hotmail.com>
2017-03-17 19:22:28 -0600

    Moved google api key into config file.

--- iHangry/src/main/java/edu/cs313/byui/ihangry/SetLocation.java
+++ iHangry/src/main/java/edu/cs313/byui/ihangry/SetLocation.java
@@ -10,11 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author Ele Thompson
- * 
- * Some TODO Items
- * - Add functionality to get the location from the browser ie. Geocoding/geolocation
- * - Store location in a session, possibly, to make it easy to access for multiple restaurant requests
- * - Figure out if its better to store location by longitude/latitude or street address
+ *
+ * Some TODO Items - Add functionality to get the location from the browser ie.
+ * Geocoding/geolocation - Store location in a session, possibly, to make it
+ * easy to access for multiple restaurant requests - Figure out if its better to
+ * store location by longitude/latitude or street address
  */
 @WebServlet(name = "SetLocation", urlPatterns = {"/SetLocation"})
 public class SetLocation extends HttpServletiHangryBase {
@@ -26,14 +26,18 @@ public class SetLocation extends HttpServletiHangryBase {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        this.LoadConfig();
+
         // Get form data from location.jsp
         String location = request.getParameter("location");
         // Replace spaces with + signs
         String parsed_location = location.replaceAll("\\s+", "+");
+
+        String ApiKey = this._config.readConfig().getProperty("googleApiKey");
         
         // Bind appropriate attributes
-        request.setAttribute("api_key", "AIzaSyBxRoQJSpiJs0YzwUoQZS86YqRT0eZSjoE");
+        request.setAttribute("api_key", ApiKey);
         request.setAttribute("parsed_location", parsed_location);
         request.setAttribute("location", location);
         // Send location data to the index.jsp
